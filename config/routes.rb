b04f640d90b889997b112529f24895323aaa5b1e
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-08-24 20:52:01 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :preferences, only: [:index]
+  end
 
   resources :artists do
     resources :songs, only: [:index, :show]
   end
   resources :songs
-
 end
